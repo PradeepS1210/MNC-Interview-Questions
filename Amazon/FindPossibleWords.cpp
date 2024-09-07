aeaeec40e4e442c5c9933d0ddd6b2ff7bda89fa8
PradeepS1210 <sooryavansheepradeep2005@gmail.com>
2024-09-07 07:55:05 +0530

    Add Solution to Find Possible Words

---
+++ Amazon/FindPossibleWords.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canFormWord(string word, unordered_map<char, int> charCount)
+{
+    for (char ch : word)
+    {
+        if (charCount[ch] > 0)
+        {
+            charCount[ch]--;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<string> W(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> W[i];
+    }
+
+    string C;
+    cin >> C;
+
+    unordered_map<char, int> charCount;
+    for (char ch : C)
+    {
+        charCount[ch]++;
+    }
+
+    int totalLength = 0;
+
+    for (const string &word : W)
+    {
+        if (canFormWord(word, charCount))
+        {
+            totalLength += word.length();
+        }
+    }
+
+    cout << totalLength << endl;
+
+    return 0;
+}
